0974febb013e643cbdc73bb2990e56afda48e37b
MarielaSolanoG <128342193+MarielaSolanoG@users.noreply.github.com>
2024-11-09 14:19:08 -0600

    se agrega crear proyecto

--- scripts/main_page.js
+++ scripts/main_page.js
@@ -1,13 +1,63 @@
+// Función que alterna la clase "menu-open" en el elemento <body>
 function toggleMenu() {
     document.body.classList.toggle("menu-open");
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const accountSettingsLink = document.getElementById('account-settings-link');
-    const wrapper = document.querySelector('.wrapper');
+// Función para manejar el botón de carga de archivo
+function handleFileUpload() {
+    const uploadButton = document.getElementById("upload-btn");
+    const fileInput = document.getElementById("file-input");
 
-    accountSettingsLink.addEventListener('click', (event) => {
-        event.preventDefault();  // Evita el salto de la página
-        wrapper.classList.toggle('hidden');  // Muestra/oculta el contenedor
+    uploadButton.addEventListener("click", function() {
+        fileInput.click(); // Esto abrirá el explorador de archivos
     });
+
+    // Maneja la selección de archivos
+    fileInput.addEventListener("change", function(event) {
+        const files = event.target.files;
+        if (files.length > 0) {
+            console.log("Archivo seleccionado:", files[0].name);
+        }
+    });
+}
+
+// Función para manejar la visibilidad de formularios según el enlace clicado
+function handleFormsVisibility() {
+    const links = [
+        { link: document.getElementById('account-settings-link'), form: document.getElementById('wrapper-data-edit') },
+        { link: document.getElementById('create-projects-link'), form: document.getElementById('wrapper-create-project') },
+    ];
+
+    // Función para ocultar todos los formularios
+    function hideAllForms() {
+        links.forEach(item => {
+            item.form.classList.add('hidden'); // Añade la clase 'hidden' para ocultar cada formulario
+        });
+    }
+
+    // Asigna el evento a cada enlace
+    links.forEach(item => {
+        item.link.addEventListener('click', (event) => {
+            event.preventDefault();
+            hideAllForms(); // Oculta todos los formularios
+            item.form.classList.toggle('hidden'); // Muestra el formulario asociado al enlace clicado
+        });
+    });
+}
+
+// Función para manejar el evento de cierre de sesión
+function handleLogout() {
+    const logoutIcon = document.querySelector('.logout-icon');
+    logoutIcon.addEventListener('click', (event) => {
+        event.preventDefault();  // Evita el comportamiento predeterminado del enlace
+        console.log('Cerrar sesión');
+        window.location.href = 'login.html'; // Redirige al login
+    });
+}
+
+// Inicialización de los eventos una vez que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', () => {
+    handleFileUpload();
+    handleFormsVisibility();
+    handleLogout();
 });
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,112 +1,184 @@
-// Selección de formularios y elementos de entrada comunes
-const form = document.getElementById('form-signup') || document.getElementById('form-login') || document.getElementById('form-edit');
-const email_input = document.getElementById('email-input');
-const password_input = document.getElementById('password-input');
-const firstname_input = document.getElementById('firstname-input');
-const identification_input = document.getElementById('identification-input');
-const telephone_input = document.getElementById('telephone-input');
-const wallet_input = document.getElementById('wallet-input');
-const work_area_input = document.getElementById('work-area');
-const repeat_password_input = document.getElementById('repeat-password-input');
-const error_message = document.getElementById('error-message');
+document.addEventListener('DOMContentLoaded', function() {
+    // Selección de formularios y elementos de entrada comunes
+    const form = document.getElementById('form-signup') || document.getElementById('form-login');
+    const formCreateProject = document.getElementById('form-create-project');
+    const formEditDataUser = document.getElementById('form-edit');
+    
+    /* USER */
+    const emailInput = document.getElementById('email-input');
+    const passwordInput = document.getElementById('password-input');
+    const firstnameInput = document.getElementById('firstname-input');
+    const identificationInput = document.getElementById('identification-input');
+    const telephoneInput = document.getElementById('telephone-input');
+    const walletInput = document.getElementById('wallet-input');
+    const workAreaInput = document.getElementById('work-area');
+    const repeatPasswordInput = document.getElementById('repeat-password-input');
+    
+    /* PROJECT */
+    const projectNameInput = document.getElementById('project-name-input');
+    const descriptionInput = document.getElementById('description-input');
+    const objectiveInput = document.getElementById('objective-input');
+    const dateInput = document.getElementById('date-input');
+    
+    const errorMessage = document.getElementById('error-message');
+    const errorMessageCreate = document.getElementById('error-message-create');
+    const errorMessageEdit = document.getElementById('error-message-edit');
+    
+    // Verificar si el formulario está presente antes de agregar el eventListener
+    if (form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let errors = [];
 
-// Agregar evento de escucha para el envío del formulario
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let errors = [];
+            if (form.id === 'form-signup') {
+                errors = validateSignupForm();
+            } else if (form.id === 'form-login') {
+                errors = validateLoginForm();
+                // Redirigir si no hay errores
+                if (errors.length === 0) {
+                    window.location.href = "main_page_user.html";
+                    return;
+                }
+            }
+            if (errors.length > 0) {
+                errorMessage.classList.add('error-message');
+                errorMessage.innerText = errors.join(". ");
+            }else{
+                form.submit()
+            }
+        });
+    }
 
-    if (form.id === 'form-signup') {
-        errors = validateSignupForm();
-    } else if (form.id === 'form-login') {
-        errors = validateLoginForm();
-        // Redirigir si no hay errores
-        if (errors.length === 0) {
-            window.location.href = "main_page_user.html";
-            return;
+    // Verificar si el formulario de crear proyecto está presente antes de agregar el eventListener
+    if (formCreateProject) {
+        formCreateProject.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let errors = [];
+            errors = validateCreateProject();
+            console.log('Edit Create Errors:', errors); // Depuración
+            if (errors.length > 0) {
+                errorMessageCreate.classList.add('error-message');
+                errorMessageCreate.innerText = errors.join(". ");
+            }else{
+                formCreateProject.submit()
+            }
+        });
+    }
+
+    // Verificar si el formulario de editar datos está presente antes de agregar el eventListener
+    if (formEditDataUser) {
+        formEditDataUser.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let errors = [];
+            errors = validateEditDataForm();
+            console.log('Edit Data User Errors:', errors); // Depuración
+            if (errors.length > 0) {
+                errorMessageEdit.classList.add('error-message');
+                errorMessageEdit.innerText = errors.join(". ");
+            }else{
+                formEditDataUser.submit()
+            }
+        });
+    }
+
+    // Función para validar el campo vacío o nulo
+    function validateField(input, message) {
+        if (!input || !input.value || input.value.trim() === '') {
+            input?.parentElement.classList.add('incorrect');
+            return message;
         }
-    } else if (form.id === 'form-edit') {
-        errors = validateEditDataForm();
+        return null;
     }
 
-    if (errors.length > 0) {
-        error_message.innerText = errors.join(". ");
-    }
-});
+    // Validación para el formulario de registro
+    function validateSignupForm() {
+        const errors = [];
+        errors.push(
+            validateField(firstnameInput, 'Nombre es requerido'),
+            validateField(emailInput, 'Email es requerido'),
+            validateField(passwordInput, 'Contraseña es requerida'),
+            validateField(identificationInput, 'Cédula es requerida'),
+            validateField(telephoneInput, 'Teléfono es requerido'),
+            validateField(walletInput, 'Billetera es requerida'),
+            validateField(workAreaInput, 'Área de trabajo es requerida')
+        );
 
-// Función para validar el campo vacío o nulo
-function validateField(input, message) {
-    if (!input || !input.value || input.value.trim() === '') {
-        input?.parentElement.classList.add('incorrect');
-        return message;
-    }
-    return null;
-}
+        if (passwordInput?.value.length < 6) {
+            errors.push('La contraseña debe tener al menos 6 caracteres');
+            passwordInput.parentElement.classList.add('incorrect');
+        }
 
-// Validación para el formulario de registro
-function validateSignupForm() {
-    const errors = [];
-    errors.push(
-        validateField(firstname_input, 'Nombre es requerido'),
-        validateField(email_input, 'Email es requerido'),
-        validateField(password_input, 'Contraseña es requerida'),
-        validateField(identification_input, 'Cédula es requerida'),
-        validateField(telephone_input, 'Teléfono es requerido'),
-        validateField(wallet_input, 'Billetera es requerida'),
-        validateField(work_area_input, 'Área de trabajo es requerida')
-    );
+        if (passwordInput?.value !== repeatPasswordInput?.value) {
+            errors.push('Las contraseñas no coinciden');
+            passwordInput.parentElement.classList.add('incorrect');
+            repeatPasswordInput.parentElement.classList.add('incorrect');
+        }
 
-    if (password_input?.value.length < 6) {
-        errors.push('La contraseña debe tener al menos 6 caracteres');
-        password_input.parentElement.classList.add('incorrect');
+        return errors.filter(error => error !== null);
     }
 
-    if (password_input?.value !== repeat_password_input?.value) {
-        errors.push('Las contraseñas no coinciden');
-        password_input.parentElement.classList.add('incorrect');
-        repeat_password_input.parentElement.classList.add('incorrect');
+    // Validación para el formulario de inicio de sesión
+    function validateLoginForm() {
+        const errors = [];
+        errors.push(
+            validateField(emailInput, 'El correo electrónico es requerido'),
+            validateField(passwordInput, 'La contraseña es requerida')
+        );
+
+        if (passwordInput && passwordInput.value.length < 6) {
+            errors.push('La contraseña debe tener al menos 6 caracteres');
+            passwordInput.parentElement.classList.add('incorrect');
+        }
+
+        return errors.filter(error => error !== null);
     }
 
-    return errors.filter(error => error !== null);
-}
+    // Validación para el formulario de edición de datos
+    function validateEditDataForm() {
+        const errors = [];
+        errors.push(
+            validateField(firstnameInput, 'Nombre es requerido'),
+            validateField(emailInput, 'Email es requerido'),
+            validateField(identificationInput, 'Cédula es requerida'),
+            validateField(telephoneInput, 'Teléfono es requerido'),
+            validateField(walletInput, 'Billetera es requerida'),
+            validateField(workAreaInput, 'Área de trabajo es requerido')
+        );
 
-// Validación para el formulario de inicio de sesión
-function validateLoginForm() {
-    const errors = [];
-    errors.push(
-        validateField(email_input, 'El correo electrónico es requerido'),
-        validateField(password_input, 'La contraseña es requerida')
-    );
-
-    if (password_input && password_input.value.length < 6) {
-        errors.push('La contraseña debe tener al menos 6 caracteres');
-        password_input.parentElement.classList.add('incorrect');
+        return errors.filter(error => error !== null);
     }
 
-    return errors.filter(error => error !== null);
-}
+    // Validación para el formulario de creación de proyecto
+    function validateCreateProject() {
+        const errors = [];
+        errors.push(
+            validateField(projectNameInput, 'Nombre es requerido'),
+            validateField(descriptionInput, 'Descripción es requerido'),
+            validateField(objectiveInput, 'Objetivo es requerido'),
+            validateField(dateInput, 'Fecha es requerida'),
+        );
 
-// Validación para el formulario de edición de datos
-function validateEditDataForm() {
-    const errors = [];
-    errors.push(
-        validateField(firstname_input, 'Nombre es requerido'),
-        validateField(email_input, 'Email es requerido'),
-        validateField(identification_input, 'Cédula es requerida'),
-        validateField(telephone_input, 'Teléfono es requerido'),
-        validateField(wallet_input, 'Billetera es requerida'),
-        validateField(work_area_input, 'Área de trabajo es requerida')
-    );
+        return errors.filter(error => error !== null);
+    }
 
-    return errors.filter(error => error !== null);
-}
+    // Limpiar mensajes de error y clases incorrectas al modificar los campos
+    const allInputs = [firstnameInput, emailInput, passwordInput, repeatPasswordInput, identificationInput, telephoneInput, walletInput, workAreaInput,
+        projectNameInput, descriptionInput, objectiveInput, dateInput]
+      .filter(input => input != null);
 
-// Limpiar mensajes de error y clases incorrectas al modificar los campos
-const allInputs = [firstname_input, email_input, password_input, repeat_password_input, identification_input, telephone_input, wallet_input, work_area_input]
-  .filter(input => input != null);
-
-allInputs.forEach(input => {
-    input.addEventListener('input', () => {
-        input.parentElement.classList.remove('incorrect');
-        error_message.innerText = '';
+    allInputs.forEach(input => {
+        input.addEventListener('input', () => {
+            input.parentElement.classList.remove('incorrect');
+            // Limpiar los mensajes de error solo si el elemento existe
+            if (errorMessage) {
+                errorMessage.innerText = '';
+            }
+            if (errorMessageCreate) {
+                errorMessageCreate.innerText = '';
+            }
+            if (errorMessageEdit) {
+                errorMessageEdit.innerText = '';
+            }
+        });
     });
 });
