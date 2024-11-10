7be6b01056744b058871c0a65e2a7e41190684e1
DavidBrenes <dbrenesmartinez@gmail.com>
2024-11-09 20:29:01 -0600

    Update navigationHeader.js

--- scripts/navigationHeader.js
+++ scripts/navigationHeader.js
@@ -3,33 +3,36 @@ function fn_navHeader_toggleMenu() {
     document.body.classList.toggle("navHeader_menu-open");
 }
 
+const links = [
+    { nav_option_name: 'Creación de Proyectos', nav_option_id: 'create-projects-link', link: 'main_page_user.html', formId: 'wrapper-create-project' },
+    { nav_option_name: 'Edición de Proyectos', nav_option_id: 'edit-projects-link', link: 'main_page_user.html', formId: 'wrapper-edit-project' },
+    { nav_option_name: 'Listado de Proyectos', nav_option_id: 'list-projects-link', link: 'listadoProyectos.html' },
+    { nav_option_name: 'Historial de Donaciones', nav_option_id: 'history-donations-link', link: 'historialDonaciones.html' },
+    { nav_option_name: 'Configuración de la cuenta', nav_option_id: 'account-settings-link', link: 'main_page_user.html', formId: 'wrapper-data-edit' }
+];
+
 // Función para manejar la visibilidad de formularios según el enlace clicado
-function fn_navHeader_handleFormsVisibility() {
-    const links = [
-        { nav_option: document.getElementById('account-settings-link'), link:'main_page_user.html',     formId: 'wrapper-data-edit' },
-        { nav_option: document.getElementById('create-projects-link'),  link:'main_page_user.html',     formId: 'wrapper-create-project' },
-        { nav_option: document.getElementById('edit-projects-link'),    link:'main_page_user.html',     formId: 'wrapper-edit-project' },
-        { nav_option: document.getElementById('list-projects-link'),    link:'listadoProyectos.html',   },
-        { nav_option: document.getElementById('history-donations-link'),link:'historialDonaciones.html',},
-    ];
+async function fn_navHeader_handleClick() {
+    // Espera a que los elementos estén presentes en el DOM
+    await new Promise(resolve => setTimeout(resolve, 0));
 
-
-    // Asigna el evento a cada enlace
     links.forEach(item => {
-        item.nav_option.addEventListener('click', (event) => {
-            event.preventDefault();
-            if (item.formId) window.location.href = `${item.link}?form=${item.formId}`;
-            else window.location.href = `${item.link}`;
-        });
+        const element = document.getElementById(item.nav_option_id);
+        if (element) {
+            element.addEventListener('click', (event) => {
+                event.preventDefault();
+                if (item.formId) window.location.href = `${item.link}?form=${item.formId}`;
+                else window.location.href = `${item.link}`;
+            });
+        }
     });
 }
-
 
 // Función para manejar el evento de cierre de sesión
 function fn_navHeader_handleLogout() {
     const logoutIcon = document.querySelector('.navHeader_logout-icon');
     logoutIcon.addEventListener('click', (event) => {
-        event.preventDefault();  // Evita el comportamiento predeterminado del enlace
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
         console.log('Cerrar sesión');
         window.location.href = 'login.html'; // Redirige al login
     });
@@ -47,6 +50,8 @@ function fn_navHeader_loadStylesheet() {
 function fn_navHeader_insertHeaderBar() {
     const headerBar = document.createElement('div');
     headerBar.className = 'navHeader_header-bar';
+    const navLinks = links.map(link => `<a id="${link.nav_option_id}">${link.nav_option_name}</a>`).join('');
+
     headerBar.innerHTML = `
         <div class="navHeader_menu-toggle" onclick="fn_navHeader_toggleMenu()">
             <span></span>
@@ -54,11 +59,7 @@ function fn_navHeader_insertHeaderBar() {
             <span></span>
         </div>
         <nav class="navHeader_menu">
-            <a id="create-projects-link">Creación de Proyectos</a>
-            <a id="edit-projects-link">Edición de Proyectos</a>
-            <a id="list-projects-link">Listado de Proyectos</a>
-            <a id="history-donations-link">Historial de Donaciones</a>
-            <a id="account-settings-link">Configuración de la cuenta</a>
+            ${navLinks}
         </nav>
         <a href="#logout" class="navHeader_logout-icon" title="Salir de la aplicación">
             <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960" width="35px" fill="#e8eaed">
@@ -70,9 +71,9 @@ function fn_navHeader_insertHeaderBar() {
 }
 
 // Inicialización de los eventos una vez que el DOM esté cargado
-document.addEventListener('DOMContentLoaded', () => {
-    fn_navHeader_loadStylesheet();
+document.addEventListener('DOMContentLoaded', async () => {
     fn_navHeader_insertHeaderBar();
-    fn_navHeader_handleFormsVisibility();
+    fn_navHeader_loadStylesheet();
+    await fn_navHeader_handleClick();
     fn_navHeader_handleLogout();
 });
